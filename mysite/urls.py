a125234f00a62102a3ce63fcc55537f9171d8a0a
vignesh <vignesh.feb2092@gmail.com>
2017-05-16 13:42:08 +0530

    added app files and folder

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+
+    url(r'^first/', include('mysite2.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
