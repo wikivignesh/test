7d46b26ac595c71160a4c79d9ebe8241004735f9
vignesh <vignesh.feb2092@gmail.com>
2017-05-23 16:28:24 +0530

    removed app url

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
 
-    url(r'^first/', include('mysite2.urls')),
+    #url(r'^first/', include('mysite2.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
